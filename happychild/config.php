327af57f08b996c88c769d0dd202f55011f4a805
Yulkin <bj-77@ya.ru>
2019-11-20 16:05:48 +0300

    ДЗ № 6.

---
+++ happychild/config.php
@@ -0,0 +1,4 @@
+<?php
+  define("IMG_BIG", "img/img_big/");
+  define("IMG_SMALL", "img/img_small/");
+?>
---
+++ happychild/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Детские игрушки</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="logo">
+                <img src="img/logo.jpg" alt="Logo">
+            </div>
+            <ul class='menu'>
+                <li><a href="index.php">Главная</a></li>
+                <li><a href="catalog.php">Каталог</a></li>
+                <li><a href="contacts.php">Контакты</a></li>
+                <li><a href="registration.php">Регистрация</a></li>
+            </ul>
+        </div>
+        <hr>
+        <div class="content">
+            <h3>Интернет-магазин игрушек для детей</h3>
+            <p class="text">На страницах нашего интернет-магазина мы рады предложить Вашему ребенку большой, постоянно пополняющийся ассортимент детских <a href="#">игрушек</a> от ведущих мировых производителей и полюбившихся Вам российских брендов. У нас представлен большой и разнообразный ассортимент игрушек по очень выгодным ценам.</p>
+        </div>
+        <hr>
+        <div class="footer">
+            <footer>&copy; Все права защищены</footer>
+        </div>
+    </div>
+</body></html>
